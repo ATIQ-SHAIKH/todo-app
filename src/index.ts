23ee4e5f0a747c677903552bb189e0209f5cda9f
atiq-aib <atiq@aibuddha.in>
2023-07-12 16:18:20 +0530

    🎉  Initial Setup, need to apply better css

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,75 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+type Task = {
+  title: string
+  completed: boolean
+  createdAt: Date
+}
 
-import confetti from 'canvas-confetti';
+const loadTasks = (): Task[] => {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null || taskJSON == "" || taskJSON == undefined) return []
+  return JSON.parse(taskJSON)
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const saveTasks = () => {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+
+const addListItem = (task: Task) => {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed;
+  const span = document.createElement("span")
+  span.className = "checkmark";
+  label.className = "labelContainer"
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("form") as HTMLFormElement | null
+const button = document.getElementById("clear") 
+const input = document.querySelector<HTMLInputElement>("#input")
+let tasks: Task[] = loadTasks()
+tasks.forEach((task)=>addListItem(task))
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return
+
+  const newTask: Task = {
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = ""
+})
+
+// const clear = () => {
+//   console.log("h")
+//   localStorage.setItem("TASKS", "");
+//   console.log(localStorage.getItem("TASKS"))
+// }
+
+button?.addEventListener("click",(e)=>{
+  localStorage.clear()
+  tasks = []
+  const listItems = document.querySelectorAll('#list');
+  listItems.forEach(listItem => {
+    listItem.parentNode?.removeChild(listItem);
+  });
+  
+  
+})
